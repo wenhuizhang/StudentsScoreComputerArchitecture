9bbce44289b6cc9d8d119398d4dfeb1eb94731fa
wenhuizhang <wenhui@gwmail.gwu.edu>
2015-02-23 19:47:07 -0800

    Update StudentScore.py

--- Python/StudentScore.py
+++ Python/StudentScore.py
@@ -1,20 +1,20 @@
 lloyd = {
     "name": "Lloyd",
     "homework": [90.0, 97.0, 75.0, 92.0],
-    "quizzes": [88.0, 40.0, 94.0],
-    "tests": [75.0, 90.0]
+    "attendence": [88.0, 40.0, 94.0],
+    "papers": [75.0, 90.0]
 }
 alice = {
     "name": "Alice",
     "homework": [100.0, 92.0, 98.0, 100.0],
-    "quizzes": [82.0, 83.0, 91.0],
-    "tests": [89.0, 97.0]
+    "attendence": [82.0, 83.0, 91.0],
+    "papers": [89.0, 97.0]
 }
 tyler = {
     "name": "Tyler",
     "homework": [0.0, 87.0, 75.0, 22.0],
-    "quizzes": [0.0, 75.0, 78.0],
-    "tests": [100.0, 100.0]
+    "attendence": [0.0, 75.0, 78.0],
+    "papers": [100.0, 100.0]
 }
 
 
@@ -29,25 +29,29 @@ def average(numbers):
 def get_average(student):
 
     Homework = average(student["homework"])
-    Quizzes = average(student["quizzes"])
-    Tests = average(student["tests"])
+    Attendences = average(student["attendence"])
+    Papers = average(student["papers"])
+    
+    '''
+    change your assign weight here
+    '''
 
-    return Homework * 0.1 + Quizzes * 0.3 + Tests * 0.6
+    return Homework * 0.4 + Attendences * 0.0 + Papers * 0.6
 
 
 def get_letter_grade(score):
-    if score >= 90: 
+    if score >= 80: 
         return "A"
-    elif score >= 80:
-        return "B"
     elif score >= 70:
-        return "C"
+        return "B"
     elif score >= 60:
+        return "C"
+    elif score >= 50:
         return "D"
     else:
         return "F"
         
-get_letter_grade(get_average(lloyd))        
+#get_letter_grade(get_average(lloyd))        
     
 
 def get_class_average(students):
@@ -60,7 +64,9 @@ def get_class_average(students):
     '''
     return average(results)  
     
-    
+'''
+student list here
+'''
     
 students = [lloyd, alice, tyler]    
 get_class_average(students)   
